b4b1ee1346fb489180bcfbf9e9755ea597705285
estefany <1031estefa@gmail.com>
2026-04-30 18:56:56 -0500

    clase NotificationChannel, de abstraccion

--- app/model/notification.py
+++ app/model/notification.py
@@ -1,6 +1,23 @@
 import os
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Protocol, runtime_checkable
 
 from app.services.util import generate_unique_id
+
+class NotificationChannel(ABC):
+
+    @abstractmethod
+    def send(self, message: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_channel_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def is_available(self) -> bool:
+        pass
+
+

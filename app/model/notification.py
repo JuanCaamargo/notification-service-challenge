8aaa705680af3956d4e0d3197a5b822caf1beab8
JuanCaamargo <xltzix07@gmail.com>
2026-04-30 19:03:04 -0500

    implementacion de la clase ConsoleChannel

--- app/model/notification.py
+++ app/model/notification.py
@@ -1,6 +1,24 @@
 import os
 from dataclasses import dataclass, field
 from datetime import datetime
+from email import message
 from typing import Protocol, runtime_checkable
 
 from app.services.util import generate_unique_id
+
+class ConsoleChannel(NotificationChannel):
+
+    def send(self, message: str) -> None:
+        if not self.is_available():
+            raise ChannelUnavailableError()
+
+        try:
+            print(message)
+        except Exception:
+            raise DeliveryError()
+
+    def get_channel_name(self) -> str:
+        return "Console"
+
+    def is_available(self) -> bool:
+        return True
